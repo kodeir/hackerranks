f99cfde2d45e6267c2fd4a817815c6ca8f7b8736
kodeir <kodeir@gmail.com>
2017-09-08 15:08:04 +0300

    first solution

--- src/main/java/javadatastructures/Java2DArray.java
+++ src/main/java/javadatastructures/Java2DArray.java
@@ -6,13 +6,26 @@ package javadatastructures;
  */
 class Java2DArray {
 
-    int array[][];
+    private int array[][];
 
     Java2DArray(int array[][]){
         this.array = array;
     }
 
     int findBiggestHourglass(){
-        return 0;
+        int biggestSum = 0;
+
+        for (int i=0; i<array.length-2; i++){
+            for (int j=0; j<array[i].length-2;j++){
+                int hourglass = array[i][j] + array[i][j+1] + array[i][j+2]
+                                            + array[i+1][j+1]
+                            + array[i+2][j] + array[i+2][j+1] + array[i+2][j+2];
+                if (hourglass > biggestSum){
+                    biggestSum = hourglass;
+                }
+            }
+        }
+
+        return biggestSum;
     }
 }
