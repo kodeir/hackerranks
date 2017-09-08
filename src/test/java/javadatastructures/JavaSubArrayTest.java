a9cb6cb20c69254e5f338ac74a7ad57625b98098
kodeir <kodeir@gmail.com>
2017-09-08 20:13:32 +0300

    parametrized test

--- src/test/java/javadatastructures/JavaSubArrayTest.java
+++ src/test/java/javadatastructures/JavaSubArrayTest.java
@@ -1,15 +1,34 @@
 package javadatastructures;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.*;
-
+@RunWith(Parameterized.class)
 public class JavaSubArrayTest {
+
+    int array[];
+    int negativeArrayCount;
+
+    @Parameterized.Parameters
+    public static Object[][] arrays() {
+        return new Object[][]{
+                {new int[]{1}, 0},
+                {new int[]{-1, 0}, 2},
+                {new int[]{-1, 2, 0}, 1},
+                {new int[]{-5, 1, 7, 0}, 2},
+                {new int[]{2, 0, -3, 2, -5}, 10},
+        };
+    }
+
+    public JavaSubArrayTest(int array[], int negativeArrayCount){
+        this.array = array;
+        this.negativeArrayCount = negativeArrayCount;
+    }
+
     @Test
     public void countNegativeSubArrays() throws Exception {
-        int array[] = new int[]{-5,1,7,0};
-        // all subarrays are: -5 1 7 0 -4 8 7 3 8 3
-        int negativeArrayCount = 2;
         JavaSubArray javaSubArray = new JavaSubArray(array);
         assertEquals(negativeArrayCount, javaSubArray.countNegativeSubArrays());
     }
