b6289bbb89b1d0d687e24010c401ad19cfe46748
kodeir <kodeir@gmail.com>
2017-09-08 13:39:07 +0300

    null check

--- src/main/java/javabignumber/JavaBigDecimal.java
+++ src/main/java/javabignumber/JavaBigDecimal.java
@@ -16,10 +16,12 @@ public class JavaBigDecimal {
         System.out.println("Non-sorted string array: " + Arrays.toString(strings));
         Arrays.sort(strings, new Comparator<String>(){
             public int compare(String s1, String s2){
-                BigDecimal a,b;
-                a = new BigDecimal(s1);
-                b = new BigDecimal(s2);
-                return -a.compareTo(b);
+                if (s1 != null && s2 != null) {
+                    BigDecimal a, b;
+                    a = new BigDecimal(s1);
+                    b = new BigDecimal(s2);
+                    return -a.compareTo(b);
+                } else return 0;
             }
         });
         System.out.println("Sorted (reversed order) string array: " + Arrays.toString(strings));
