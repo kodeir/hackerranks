e5ac64c8212c966be4fa1880aa8f69eaf83a74ca
kodeir <kodeir@gmail.com>
2017-09-08 17:23:38 +0300

    negative sub arrays initial commit

---
+++ src/main/java/javadatastructures/JavaSubArray.java
@@ -0,0 +1,20 @@
+package javadatastructures;
+
+/**
+ * https://www.hackerrank.com/challenges/java-negative-subarray/problem
+ */
+
+public class JavaSubArray {
+
+    int array[];
+    int n;
+
+    JavaSubArray(int array[]){
+        this.array = array;
+        n = array.length;
+    }
+
+    int countNegativeSubArrays(){
+        return 0;
+    }
+}
