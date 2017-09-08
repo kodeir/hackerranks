b2e2e0414c2ce0e19217fd7abdab00e976203d83
kodeir <kodeir@gmail.com>
2017-09-08 14:42:19 +0300

    hourglasses class initial commit

---
+++ src/main/java/javadatastructures/Java2DArray.java
@@ -0,0 +1,8 @@
+package javadatastructures;
+
+/**
+ * https://www.hackerrank.com/challenges/java-2d-array/problem
+ * In this problem you have to print the largest sum among all the hourglasses in the array.
+ */
+public class Java2DArray {
+}
