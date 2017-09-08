f8c00a06acbfc3e5e61d1e65208425a8f619f7e7
kodeir <kodeir@gmail.com>
2017-09-08 13:46:29 +0300

    comments on reverse order

--- src/main/java/javabignumber/JavaBigDecimal.java
+++ src/main/java/javabignumber/JavaBigDecimal.java
@@ -14,12 +14,14 @@ public class JavaBigDecimal {
 
     public String[] doReverseSort(){
         System.out.println("Non-sorted string array: " + Arrays.toString(strings));
+        // Collections.reverseOrder(new Comparator ...); could be used
         Arrays.sort(strings, new Comparator<String>(){
             public int compare(String s1, String s2){
                 if (s1 != null && s2 != null) {
                     BigDecimal a, b;
                     a = new BigDecimal(s1);
                     b = new BigDecimal(s2);
+                    // b.compareTo(a) will also give a reverse order
                     return -a.compareTo(b);
                 } else return 0;
             }
