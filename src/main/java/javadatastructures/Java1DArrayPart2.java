ed98e43de8c6a0e7f76ad8a168562e75c1f769b5
kodeir <kodeir@gmail.com>
2017-09-11 14:23:05 +0300

    code samples from the task

--- src/main/java/javadatastructures/Java1DArrayPart2.java
+++ src/main/java/javadatastructures/Java1DArrayPart2.java
@@ -1,13 +1,17 @@
 package javadatastructures;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * https://www.hackerrank.com/challenges/java-1d-array/problem
  */
 class Java1DArrayPart2 {
 
-    int array[][];
+    private int array[][];
     //number of queries
-    int q;
+    private int q;
 
     Java1DArrayPart2(int array[][], int q){
         this.array = array;
@@ -15,6 +19,32 @@ class Java1DArrayPart2 {
     }
 
     String[] playGame(){
-        return new String[]{};
+
+        int arrayI = 0;
+        int arrayJ = 0;
+
+        String result[] = new String[q];
+
+        while (q-- > 0) {
+            int n = array[arrayI][arrayJ];
+            int leap = array[arrayI][arrayJ+1];
+
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = array[arrayI+1][arrayJ];
+            }
+
+            result[q] = ( (canWin(leap, game)) ? "YES" : "NO" );
+            arrayI = arrayI+2;
+        }
+
+        List<String> list = Arrays.asList(result);
+        Collections.reverse(list);
+        return list.toArray(new String[q]);
+    }
+
+    private boolean canWin(int leap, int[] game) {
+
+        return false;
     }
 }
