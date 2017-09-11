0cdd64e96ce1d75c095ac7106bbc5a16ef858d47
kodeir <kodeir@gmail.com>
2017-09-11 13:53:01 +0300

    move-leap game initial commit

---
+++ src/main/java/javadatastructures/Java1DArrayPart2.java
@@ -0,0 +1,20 @@
+package javadatastructures;
+
+/**
+ * https://www.hackerrank.com/challenges/java-1d-array/problem
+ */
+class Java1DArrayPart2 {
+
+    int array[];
+    //number of queries
+    int q;
+
+    Java1DArrayPart2(int array[], int q){
+        this.array = array;
+        this.q = q;
+    }
+
+    String playGame(){
+        return "";
+    }
+}
