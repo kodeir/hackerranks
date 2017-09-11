2aa0386d4873ec33ce20343031968152a4700630
kodeir <kodeir@gmail.com>
2017-09-11 13:56:26 +0300

    move-leap game initial test commit

---
+++ src/test/java/javadatastructures/Java1DArrayPart2Test.java
@@ -0,0 +1,31 @@
+package javadatastructures;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Java1DArrayPart2Test {
+    @Test
+    public void playGame() throws Exception {
+        int q = 4;
+        int array[][] = new int[][]{
+                new int[]{5,3},
+                new int[]{0,0,0,0,0},
+                new int[]{6,5},
+                new int[]{0,0,0,1,1,1},
+                new int[]{6,3},
+                new int[]{0,0,1,1,1,0},
+                new int[]{3,1},
+                new int[]{0,1,0}
+        };
+        String result[] = new String[]{
+                "YES",
+                "YES",
+                "NO",
+                "NO"
+        };
+        Java1DArrayPart2Test java1DArrayPart2Test = new Java1DArrayPart2Test(array, 4);
+        java1DArrayPart2Test.playGame();
+    }
+
+}
